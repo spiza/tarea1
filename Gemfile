210df11a25e976dbb0148bd4a9cd83c3335bc8ef
Sebastián Piza <spiza@uc.cl>
2017-03-14 15:39:28 -0300

    veamos si funciona

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+ruby '2.2.6'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -7,4 +7,14 @@ class NewsController < ApplicationController
   def show
     @new = New.find(params[:id])
   end
+  #GET /news/new
+  def new
+    @new = New.new
+  end
+  #POST news
+  def create
+    @new = New.new(titulo: params[:new][:titulo], bajada: params[:new][:bajada], cuerpo: params[:new][:cuerpo])
+    @new.save
+    redirect_to @new
+  end
 end
